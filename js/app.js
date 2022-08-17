605df48911b437920cb0f9b3d12eb02938445f96
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-08-17 15:43:58 -0700

    Catching up

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 console.log("test");
 
 const allProducts = [];
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
 // Product Constructor:
 function Product(name) {
     this.name = name;
